ef9d7e24659ecbd5087819d26b536c6d1ffac5fb
nMaax <massimilianocarli@proton.me>
2025-06-14 15:55:28 +0200

    Provided MaskedZukoODE

--- sbi/samplers/ode_solvers/zuko_ode.py
+++ sbi/samplers/ode_solvers/zuko_ode.py
@@ -11,7 +11,93 @@ from torch.distributions import Distribution
 from zuko.distributions import DiagNormal, NormalizingFlow
 from zuko.transforms import FreeFormJacobianTransform
 
-from sbi.samplers.ode_solvers.base import NeuralODE, NeuralODEFunc
+from sbi.samplers.ode_solvers.base import (
+    MaskedNeuralODE,
+    MaskedNeuralODEFunc,
+    NeuralODE,
+    NeuralODEFunc,
+)
+
+
+class MaskedZukoNeuralODE(MaskedNeuralODE):
+    def __init__(
+        self,
+        f: MaskedNeuralODEFunc,
+        net: nn.Module,
+        mean_base: Tensor,
+        std_base: Tensor,
+        t_min: float = 0.0,
+        t_max: float = 1.0,
+        atol: float = 1e-6,
+        rtol: float = 1e-5,
+        exact: bool = True,
+    ) -> None:
+        r"""
+        Initialize the ZukoNeuralODE class.
+
+        References:
+        ----------
+        .. [1] FFJORD: Free-form Continuous Dynamics for Scalable Reversible Generative
+           Models (Grathwohl et al., 2018)
+           https://arxiv.org/abs/1810.01367
+
+        Args:
+            f: The function to be integrated that implements the `NeuralODEFunc`
+                protocol. Must accept three arguments in the order:
+                - input (Tensor): The input state tensor :math:`\theta_t`
+                - times (Tensor): The time parameter tensor :math:`t`
+            net: The neural network that is used by the function :math:`f`.
+                This is never called explicitly by the NeuralODE class,
+                but is used to track the parameters of the neural network.
+            mean_base: The mean of the base distribution.
+                Expected shape: (1, theta_dim).
+            std_base: The std of the base distribution.
+                Expected shape: (1, theta_dim).
+            t_min: The minimum time value for the ODE solver.
+            t_max: The maximum time value for the ODE solver.
+            atol: The absolute tolerance for the ODE solver.
+            rtol: The relative tolerance for the ODE solver.
+            exact: Whether the exact log-determinant of the Jacobian or an unbiased
+            stochastic estimate thereof is calculated.
+        """
+
+        super().__init__(
+            f,
+            net,
+            mean_base,
+            std_base,
+            t_min,
+            t_max,
+            atol=atol,
+            rtol=rtol,
+            exact=exact,
+        )
+
+    def get_distribution(self, **kwargs) -> Distribution:
+        """
+        Get the distribution that wraps the ODE solver.
+
+        Args:
+            **kwargs: Additional arguments for the ODE solver.
+
+        Returns:
+            The distribution object with `log_prob` and
+            `sample` methods that wraps the ODE solver.
+        """
+        transform = FreeFormJacobianTransform(
+            f=lambda t, input: self.f(input, t),
+            t0=self.mean_base.new_tensor(self.t_min),
+            t1=self.mean_base.new_tensor(self.t_max),
+            phi=(*self.net.parameters(),),
+            **kwargs,
+        )
+
+        return NormalizingFlow(
+            transform=transform,
+            # ! There was an .expand(condition.shape[:-1]) here before
+            # ? Are shapes still correct?
+            base=DiagNormal(self.mean_base, self.std_base),
+        )
 
 
 class ZukoNeuralODE(NeuralODE):
