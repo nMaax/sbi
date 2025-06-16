140e0d51807ac44aa928974273e52a1a298b5160
nMaax <massimilianocarli@proton.me>
2025-06-16 11:18:06 +0200

    Added Simformer interface among importable inference classes

--- sbi/inference/__init__.py
+++ sbi/inference/__init__.py
@@ -9,7 +9,7 @@ from sbi.inference.trainers.marginal import MarginalTrainer
 from sbi.inference.trainers.nle import MNLE, NLE_A
 from sbi.inference.trainers.npe import MNPE, NPE_A, NPE_B, NPE_C  # noqa: F401
 from sbi.inference.trainers.nre import BNRE, NRE_A, NRE_B, NRE_C  # noqa: F401
-from sbi.inference.trainers.vfpe import FMPE, NPSE
+from sbi.inference.trainers.vfpe import FMPE, NPSE, Simformer
 
 SNL = SNLE = SNLE_A = NLE = NLE_A
 _nle_family = ["NLE"]
