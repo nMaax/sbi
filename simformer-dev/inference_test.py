1e57baad74ded1c245197f3c198922b5e1862ef0
nMaax <massimilianocarli@proton.me>
2025-06-22 11:29:07 +0200

    Formatted inference test file

--- simformer-dev/inference_test.py
+++ simformer-dev/inference_test.py
@@ -1,26 +1,31 @@
 # %%
 import torch
-from sbi.inference import NPE, Simformer # type: ignore
+
+from sbi.inference import Simformer  # type: ignore
 from sbi.utils import BoxUniform
 
 _ = torch.manual_seed(0)
 
-NUM_SIM_NODES = 3 # e.g., node 0 is 'theta', node 1 is 'x1', node 2 is 'x2'
-NUM_NODE_FEATURES = 3 # e.g., both theta1, x1 and x2 have 3 features
+NUM_SIM_NODES = 3  # e.g., node 0 is 'theta', node 1 is 'x1', node 2 is 'x2'
+NUM_NODE_FEATURES = 3  # e.g., both theta1, x1 and x2 have 3 features
+
 
 def simformer_simulator(num_simulations):
-
     theta1 = torch.randn(num_simulations, NUM_NODE_FEATURES) * 3.0
     x1 = 2 * torch.sin(theta1) + torch.randn(num_simulations, NUM_NODE_FEATURES) * 0.5
-    x2 = 0.1 * theta1 ** 2 + 0.5 * torch.abs(x1) * torch.randn(num_simulations, NUM_NODE_FEATURES)
+    x2 = 0.1 * theta1**2 + 0.5 * torch.abs(x1) * torch.randn(
+        num_simulations, NUM_NODE_FEATURES
+    )
 
     inputs_tensor = torch.stack([theta1, x1, x2], dim=1)
 
     # True for observed, False for latent.
     # Note: This mask is for a single 'input' sample. It gets broadcasted for the batch.
     condition_mask_single_sample = torch.zeros((NUM_SIM_NODES,), dtype=torch.bool)
-    condition_mask_single_sample[1] = True # Node 1 (index 1) is observed
-    condition_masks = condition_mask_single_sample.unsqueeze(0).expand(num_simulations, NUM_SIM_NODES)
+    condition_mask_single_sample[1] = True  # Node 1 (index 1) is observed
+    condition_masks = condition_mask_single_sample.unsqueeze(0).expand(
+        num_simulations, NUM_SIM_NODES
+    )
 
     # True for an edge from row to column.
     # ! This cause nan/inf values in attention layer within the block since mask was non-meaningful
@@ -28,10 +33,15 @@ def simformer_simulator(num_simulations):
     # edge_mask_single_sample[0, 1] = True # Edge from node 0 to node 1
 
     # ! This solves the issue (no mask)
-    edge_mask_single_sample = torch.ones((NUM_SIM_NODES, NUM_SIM_NODES), dtype=torch.bool)
-    edge_masks = edge_mask_single_sample.unsqueeze(0).expand(num_simulations, NUM_SIM_NODES, NUM_SIM_NODES)
+    edge_mask_single_sample = torch.ones(
+        (NUM_SIM_NODES, NUM_SIM_NODES), dtype=torch.bool
+    )
+    edge_masks = edge_mask_single_sample.unsqueeze(0).expand(
+        num_simulations, NUM_SIM_NODES, NUM_SIM_NODES
+    )
 
     return inputs_tensor, condition_masks, edge_masks
+
 
 # %%
 
@@ -52,15 +62,17 @@ inference: Simformer = Simformer(
 
 # %%
 num_simulations = 2000
-sim_inputs, sim_conditioning_masks, sim_edge_masks = simformer_simulator(num_simulations)
-print("sim_inputs.shape", sim_inputs.shape) # Expected: [2000, 2, 3]
-print("sim_conditioning_masks.shape", sim_conditioning_masks.shape) # Expected: [2, 3]
-print("sim_edge_masks.shape", sim_edge_masks.shape) # Expected: [2, 2]
+sim_inputs, sim_condition_masks, sim_edge_masks = simformer_simulator(
+    num_simulations
+)
+print("sim_inputs.shape", sim_inputs.shape)  # Expected: [2000, 2, 3]
+print("sim_conditioning_masks.shape", sim_condition_masks.shape)  # Expected: [2, 3]
+print("sim_edge_masks.shape", sim_edge_masks.shape)  # Expected: [2, 2]
 
 # %%
 inference.append_simulations(
     inputs=sim_inputs,
-    conditioning_masks=sim_conditioning_masks,
+    condition_masks=sim_condition_masks,
     edge_masks=sim_edge_masks,
 )
 
@@ -83,7 +95,7 @@ plt.show()
 # %%
 
 condition_mask_single_sample = torch.zeros((NUM_SIM_NODES,), dtype=torch.bool)
-condition_mask_single_sample[1] = True # Node 1 (index 1) is observed
+condition_mask_single_sample[1] = True  # Node 1 (index 1) is observed
 
 edge_mask_single_sample = torch.ones((NUM_SIM_NODES, NUM_SIM_NODES), dtype=torch.bool)
 
@@ -107,7 +119,7 @@ _ = pairplot(
     samples,
     limits=[[-2, 2], [-2, 2], [-2, 2]],
     figsize=(5, 5),
-    labels=[r"$\theta_1$", r"$\theta_2$", r"$\theta_3$"]
+    labels=[r"$\theta_1$", r"$\theta_2$", r"$\theta_3$"],
 )
 
 # %%
