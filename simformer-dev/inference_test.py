38c3ec5ce15693955efb5790f01bcdcb411d6d86
nMaax <massimilianocarli@proton.me>
2025-06-25 13:24:13 +0200

    Re-factored inference test script, simplier simulator, adjusted plots

--- simformer-dev/inference_test.py
+++ simformer-dev/inference_test.py
@@ -7,17 +7,21 @@ from sbi.utils import BoxUniform
 _ = torch.manual_seed(0)
 
 NUM_SIM_NODES = 4
-NUM_NODE_FEATURES = 7
+NUM_NODE_FEATURES = 2
 NUM_OBS_NODES = 2
 NUM_LAT_NODES = NUM_SIM_NODES - NUM_OBS_NODES
 
 def simformer_simulator(num_simulations):
-    theta1 = torch.randn(num_simulations, NUM_NODE_FEATURES) * 3.0
-    theta2 = torch.randn(num_simulations, NUM_NODE_FEATURES) * 1.5 + 2.0
-    x1 = 2 * torch.sin(theta1) + torch.randn(num_simulations, NUM_NODE_FEATURES) * 0.5
-    x2 = 0.1 * theta1**2 + 0.3 * theta2 + 0.5 * torch.abs(x1) * torch.randn(
-        num_simulations, NUM_NODE_FEATURES
-    )
+    theta1 = torch.randn(num_simulations, NUM_NODE_FEATURES)
+    theta2 = torch.randn(num_simulations, NUM_NODE_FEATURES)
+    #theta1 = torch.randn(num_simulations, NUM_NODE_FEATURES) * 3.0
+    #theta2 = torch.randn(num_simulations, NUM_NODE_FEATURES) * 1.5 + 2.0
+    x1 = theta1 + torch.randn(num_simulations, NUM_NODE_FEATURES) * 0.2
+    x2 = theta2 + torch.randn(num_simulations, NUM_NODE_FEATURES) * 0.2
+    # x1 = 2 * torch.sin(theta1) + torch.randn(num_simulations, NUM_NODE_FEATURES) * 0.5
+    # x2 = 0.1 * theta1**2 + 0.3 * theta2 + 0.5 * torch.abs(x1) * torch.randn(
+    #     num_simulations, NUM_NODE_FEATURES
+    # )
 
     inputs_tensor = torch.stack([theta1, theta2, x1, x2], dim=1)
 
@@ -57,7 +61,7 @@ inference: Simformer = Simformer(
 )
 
 # %%
-num_simulations = 500
+num_simulations = 1000
 sim_inputs, sim_condition_masks, sim_edge_masks = simformer_simulator(
     num_simulations
 )
@@ -96,6 +100,8 @@ condition_mask_single_sample[3] = True  # Index 3 is observed
 
 edge_mask_single_sample = torch.ones((NUM_SIM_NODES, NUM_SIM_NODES), dtype=torch.bool)
 
+# ! Take the network I have, and do the sampling from scratch
+
 posterior = inference.build_posterior(
     condition_mask=condition_mask_single_sample,
     edge_mask=edge_mask_single_sample,
@@ -103,15 +109,15 @@ posterior = inference.build_posterior(
 
 # %%
 x_obs = torch.as_tensor([
-    [0.5, -1.2, 0.8, 1.5, -1.8, -1.6, 0.1],
-    [-0.8, 0.3, 1.6, 0.9, -0.3, 0.25, 0.4],
+    [0.5,], #  0.8, 1.5, -1.8, -1.6, 0.1],
+    [-0.8,], #  1.6, 0.9, -0.3, 0.25, 0.4],
 ])
 x_obs = x_obs.view(1, -1)
 
 # %%
 
-samples = posterior.sample((100,), x=x_obs)
-samples = samples.reshape(-1, NUM_LAT_NODES, NUM_NODE_FEATURES)
+samples = posterior.sample((1000,), x=x_obs)
+# samples = samples.reshape(-1, NUM_LAT_NODES, NUM_NODE_FEATURES)
 
 print(f"{samples.shape=}")
 
@@ -123,7 +129,7 @@ from sbi.analysis import pairplot
 
 _ = pairplot(
     samples.reshape(-1, NUM_LAT_NODES * NUM_NODE_FEATURES),
-    limits=[[-2, 2]] * (NUM_LAT_NODES * NUM_NODE_FEATURES),
+    #limits=[[-10, 10]] * (NUM_LAT_NODES * NUM_NODE_FEATURES),
     figsize=(8, 8),
     labels=[fr"$\theta_{{{i+1}}}$" for i in range(NUM_LAT_NODES * NUM_NODE_FEATURES)],
 )
